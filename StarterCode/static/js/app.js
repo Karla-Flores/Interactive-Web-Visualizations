94d8a573e185be7d374e02abe05dae2ab8bc0e86
Karla-Flores <ms.kaflores@gmail.com>
2021-07-25 20:56:47 -0500

    Filter name and other arrays for id

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,40 +3,56 @@ d3.json('samples.json')
     .then(function (data) {
         init(data);
     });
+
 // Json organization
 function init(data) {
     console.log(data.names);
     console.log(data.metadata);
     console.log(data.samples);
     load_dropdown_list(data.names);
-    build_chart('940');
-}
+    build_chart('id');
+};
 
-// Dropdown using names for creating attributes option and value
+// Dropdown menu with Ids
 function load_dropdown_list(names) {
     let dropdown = document.getElementById('selDataset');
     names.forEach(function (name) {
-        let opt = document.createElement("option");
-        let att = document.createAttribute("value");
+        let opt = document.createElement('option');
+        let att = document.createAttribute('value');
         att.value = name;
         opt.setAttributeNode(att);
         opt.text = name;
         dropdown.appendChild(opt);
     })
-}
+};
+
 // Build a chart for a single sample such as 940
 function build_chart(id) {
-    console.log("build_chartfor" + id);
+    console.log('build_chartfor' + id);
     d3.json('samples.json')
         .then(function (data) {
             let names = data.names;
             let metadata = data.metadata;
             let samples = data.samples;
-            // Filter name and other arrays for id
-            
 
-            // build the chart
+            // Filter name and other arrays for id
+            let idMetadata = metadata.filter(participant=>participant.id)[0];
+            console.log(idMetadata);
+            Object.entries(idMetadata).forEach(([key,value])=>{
+                var list = samples.append('ul');
+                list.attr('class', 'list-group list-group-flush')
+                var item = list.append('li');
+
+                item.attr('class', 'list-group-item p-1 demo-text bg-transpaent');
+
+                item.text(`${key}: ${value}`);
+            }
+            )
+
+            //         var idMetadata = data.metadata.filter(participant => participant.id == id)[0];
+            //         console.log(idMetadata)
+            //         // build the chart
 
         });
-}
+    }
 
